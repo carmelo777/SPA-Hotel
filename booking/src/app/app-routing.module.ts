238693090d34a87f13bf62cfdcb8d5694f7c51c9
Carmelo Ravida <carmeloravida88@gmail.com>
2022-06-19 09:31:31 +0200

    clean

--- booking/src/app/app-routing.module.ts
+++ booking/src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/services/auth.guard';
-import { CartComponent } from './features/cart/cart.component';
-import { LoginComponent } from './features/login/login.component';
-import { NoResultsComponent } from './features/search/no-results.component';
-import { SearchComponent } from './features/search/search.component';
+
 
 const routes: Routes = [
   { path: 'search', loadChildren: () => import('./features/search/search.module').then(m => m.SearchModule ) , canActivate: [AuthGuard] },
